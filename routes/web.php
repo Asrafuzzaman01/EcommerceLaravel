4b7078c542d2ad1b38d1aeec714d2794db3bcac4
mithu <asrafuzzamamn.cse.siu@gmail.com>
2023-02-12 22:15:44 +0600

    user page template setub

--- routes/web.php
+++ routes/web.php
@@ -1,109 +1,120 @@
-<?php
-use App\Http\Controllers\Admin\ordersController;
-use App\Http\Controllers\Admin\productsController;
-use App\Http\Controllers\Admin\subcategoryController;
-use App\Http\Controllers\Admin\categoryController;
-use App\Http\Controllers\Admin\dashboardcontroller;
-use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
-use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
+    <?php
+    use App\Http\Controllers\Admin\ordersController;
+    use App\Http\Controllers\Admin\productsController;
+    use App\Http\Controllers\Admin\subcategoryController;
+    use App\Http\Controllers\Admin\categoryController;
+    use App\Http\Controllers\Admin\dashboardcontroller;
+    use App\Http\Controllers\Homecontroller;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+    use App\Http\Controllers\ProfileController;
+    use Illuminate\Foundation\Application;
+    use Illuminate\Support\Facades\Route;
+    use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+    /*
+    |--------------------------------------------------------------------------
+    | Web Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register web routes for your application. These
+    | routes are loaded by the RouteServiceProvider within a group which
+    | contains the "web" middleware group. Now create something great!
+    |
+    */
 
 
-Route::middleware(['auth'])->group(function () {
-
-    Route::controller(dashboardcontroller::class)->group(function () {
-        Route::get('/admin/dashboard', 'index')->name('admindashboard');
-
-    });
-
-    Route::controller(categoryController::class)->group(function () {
-        Route::get('/admin/add-category', 'addcategory')->name('addcategory');
-        Route::get('/admin/all-category', 'allcategory')->name('allcategory');
-        Route::post('/admin/storecategory', 'storecategory')->name('storecategory');
-
-        Route::get('/admin/edite-category/{id}', 'Editecategory')->name('editecategory');
-        Route::post('/admin/update-category', 'updatecategory')->name('updatecategory');
-        Route::get('/admin/delete-category/{id}', 'Deletcategory')->name('deletecategory');
-
+    Route::controller(Homecontroller::class)->group(function () {
+        Route::get('/','index')->name('home');
 
     });
 
 
+    // Route::get('/', function () {
+    //     return Inertia::render('user.layouts.user_template', [
+    //         'canLogin' => Route::has('login'),
+    //         'canRegister' => Route::has('register'),
+    //         'laravelVersion' => Application::VERSION,
+    //         'phpVersion' => PHP_VERSION,
+    //     ]);
+    // });
 
-    Route::controller(subcategoryController::class)->group(function () {
-        Route::get('/admin/add-subcategory', 'addsubcat')->name('addsubcategory');
-        Route::get('/admin/all-sub-category', 'allsubcat')->name('allsubcategory');
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
 
-        Route::post('/admin/storesubcategory', 'storesubcategory')->name('storesubcategory');
 
-        Route::get('/admin/edite-subcategory/{id}', 'Editesubcat')->name('editesubcategory');
-        Route::get('/admin/delete-subcategory/{id}', 'Deletcsubategory')->name('deletesubcategory');
-        Route::post('/admin/update-subcategory', 'Updatesubcategory')->name('updatesubcategory');
+
+    Route::middleware('auth')->group(function () {
+        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    });
+
+
+    Route::middleware(['auth'])->group(function () {
+
+        Route::controller(dashboardcontroller::class)->group(function () {
+            Route::get('/admin/dashboard', 'index')->name('admindashboard');
+
+        });
+
+        Route::controller(categoryController::class)->group(function () {
+            Route::get('/admin/add-category', 'addcategory')->name('addcategory');
+            Route::get('/admin/all-category', 'allcategory')->name('allcategory');
+            Route::post('/admin/storecategory', 'storecategory')->name('storecategory');
+
+            Route::get('/admin/edite-category/{id}', 'Editecategory')->name('editecategory');
+            Route::post('/admin/update-category', 'updatecategory')->name('updatecategory');
+            Route::get('/admin/delete-category/{id}', 'Deletcategory')->name('deletecategory');
+
+
+        });
+
+
+
+        Route::controller(subcategoryController::class)->group(function () {
+            Route::get('/admin/add-subcategory', 'addsubcat')->name('addsubcategory');
+            Route::get('/admin/all-sub-category', 'allsubcat')->name('allsubcategory');
+
+            Route::post('/admin/storesubcategory', 'storesubcategory')->name('storesubcategory');
+
+            Route::get('/admin/edite-subcategory/{id}', 'Editesubcat')->name('editesubcategory');
+            Route::get('/admin/delete-subcategory/{id}', 'Deletcsubategory')->name('deletesubcategory');
+            Route::post('/admin/update-subcategory', 'Updatesubcategory')->name('updatesubcategory');
+
+        });
+
+
+
+
+        Route::controller(productsController::class)->group(function () {
+            Route::get('/admin/add-product', 'addproduct')->name('addproduct');
+            Route::get('/admin/all-product', 'allproduct')->name('allproduct');
+            Route::post('/admin/storeproduct', 'Storeproduct')->name('storeproduct');
+            Route::get('/admin/editeproductimg/{id}', 'Editeproductimg')->name('editeproductimg');
+
+
+            Route::post('/admin/update-productimg/{id}', 'Updateproductimg')->name('updateproductimg');
+            Route::get('/admin/editeproduct/{id}', 'Editeproduct')->name('editeproduct');
+            Route::post('/admin/update-product/{id}', 'Updateproduct')->name('updateproduct');
+
+
+            Route::get('/admin/delete-product/{id}', 'Deleteproduct')->name('deleteproduct');
+
+        });
+
+        Route::controller(ordersController::class)->group(function () {
+            Route::get('/admin/pending-orders', 'pendingord')->name('pendingorders');
+            Route::get('/admin/completed-orders', 'completeord')->name('completeorders');
+            Route::get('/admin/cancel-orders', 'cancelord')->name('cancelorders');
+
+        });
+
 
     });
 
 
-
-
-    Route::controller(productsController::class)->group(function () {
-        Route::get('/admin/add-product', 'addproduct')->name('addproduct');
-        Route::get('/admin/all-product', 'allproduct')->name('allproduct');
-        Route::post('/admin/storeproduct', 'Storeproduct')->name('storeproduct');
-        Route::get('/admin/editeproductimg/{id}', 'Editeproductimg')->name('editeproductimg');
-
-
-        Route::post('/admin/update-productimg/{id}', 'Updateproductimg')->name('updateproductimg');
-        Route::get('/admin/editeproduct/{id}', 'Editeproduct')->name('editeproduct');
-        Route::post('/admin/update-product/{id}', 'Updateproduct')->name('updateproduct');
-
-
-        Route::get('/admin/delete-product/{id}', 'Deleteproduct')->name('deleteproduct');
-
-    });
-
-    Route::controller(ordersController::class)->group(function () {
-        Route::get('/admin/pending-orders', 'pendingord')->name('pendingorders');
-        Route::get('/admin/completed-orders', 'completeord')->name('completeorders');
-        Route::get('/admin/cancel-orders', 'cancelord')->name('cancelorders');
-
-    });
-
-
-});
-
-
-require __DIR__.'/auth.php';
+    require __DIR__.'/auth.php';
 
 
 
